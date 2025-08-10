a7e30d0ccd8ec16eca8e986e1350c22adad29f43
THUSLLL <vercosusteno@mail.com>
2025-08-10 17:42:19 +0800

    liscence

--- tgs/models/Fuse_Attention.py
+++ tgs/models/Fuse_Attention.py
@@ -1,3 +1,21 @@
+# Copyright 2024 VAST AI Research
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Modified by Bo Liu, 2025
+# - Added adaptive density control
+# - Improved rendering stability
+
 from diffusers.models.attention import Attention
 from diffusers.models.attention_processor import AttnProcessor2_0
 from tgs.utils.typing import *
